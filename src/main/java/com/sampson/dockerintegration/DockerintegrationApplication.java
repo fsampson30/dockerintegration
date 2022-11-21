1929cead40ebea70ab93d91635c84501cb0ad429
fsampson30 <fsampson30@gmail.com>
2022-11-21 08:13:37 -0300

    Initial commit

---
+++ src/main/java/com/sampson/dockerintegration/DockerintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.sampson.dockerintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampson/dockerintegration/DockerintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.dockerintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
