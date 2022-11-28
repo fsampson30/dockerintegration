2491c9512caf5cb394dba88bee6c58350878c78d
Sampson <fsampson30@gmail.com>
2022-11-28 16:22:07 -0300

    Changed mapping.

--- src/main/java/com/sampson/dockerintegration/controller/PersonController.java
+++ src/main/java/com/sampson/dockerintegration/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/persons")
 public class PersonController {
 
     @Autowired
