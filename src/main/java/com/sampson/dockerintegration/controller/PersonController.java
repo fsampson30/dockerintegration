408b7ee05cab643aeafa8504f215fb6ae56b4fb7
fsampson30 <fsampson30@gmail.com>
2022-12-02 07:59:54 -0300

    Implemented method find Person by id.

--- src/main/java/com/sampson/dockerintegration/controller/PersonController.java
+++ src/main/java/com/sampson/dockerintegration/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.sampson.dockerintegration.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class PersonController {
     @GetMapping
     public List<Person> getPerson(){
         return personService.getAllPersons();
+    }
+
+    @GetMapping
+    public Person getPersonById(@RequestParam long id){
+        return personService.getPersonById(id);
     }
 }
