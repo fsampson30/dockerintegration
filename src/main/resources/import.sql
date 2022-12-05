7d36367b8310f279f4327e044642c51a6bb83475
fsampson30 <fsampson30@gmail.com>
2022-12-05 10:42:28 -0300

    Removed unused import.

--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tb_persons(name) VALUES ('Flavio Sampson');
-INSERT INTO tb_persons(name) VALUES ('Juliana Golubi');
-INSERT INTO tb_persons(name) VALUES ('Fernanda Sampson');
-INSERT INTO tb_persons(name) VALUES ('Natalia Golubi');
